6113a03be0a6ece0c72d9dd284e42795e6713393
Femi Fadayomi <femzyfadayomi@gmail.com>
2024-06-30 22:49:37 +0100

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
@@ -17,7 +18,7 @@ import FeesPayment from './components/FeesPayment';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />}> </Route>
           <Route path="/signup" element={<SignUp />}> </Route>
@@ -28,7 +29,7 @@ function App() {
           <Route path="/feespayment" element={<FeesPayment />}> </Route>
         </Routes>
        <ToastContainer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
